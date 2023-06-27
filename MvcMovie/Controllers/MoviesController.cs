9147a95f18eb39fbcc4703d419ad9b942dacb820
Lukam90 <lucien.hamm@gmail.com>
2023-06-27 23:40:35 +0200

    MAJ Ex Movies > New Actions

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -13,9 +13,17 @@ public class MoviesController : Controller
     }
 
     //GET Movies
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string searchString)
     {
-        return View(await _context.Movie.ToListAsync());
+        var movies = from m in _context.Movie
+                 select m;
+
+        if (! String.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.Contains(searchString));
+        }
+
+        return View(await movies.ToListAsync());
     }
 
     //GET Movies/Details/5
@@ -32,6 +40,29 @@ public class MoviesController : Controller
         if (movie == null)
         {
             return NotFound();
+        }
+
+        return View(movie);
+    }
+
+    // GET: Movies/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Movies/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(movie);
+
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
         }
 
         return View(movie);
@@ -57,19 +88,52 @@ public class MoviesController : Controller
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!MovieExists(movie.Id))
-                {
-                    return NotFound();
-                }
-                else
+                if (movie == null)
                 {
                     throw;
                 }
+                else
+                {
+                    return NotFound();
+                }
             }
-            
+
             return RedirectToAction(nameof(Index));
         }
 
         return View(movie);
+    }
+
+    // GET: Movies/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _context.Movie
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return View(movie);
+    }
+
+    // POST: Movies/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var movie = await _context.Movie.FindAsync(id);
+
+        _context.Movie.Remove(movie);
+        
+        await _context.SaveChangesAsync();
+        
+        return RedirectToAction(nameof(Index));
     }
 }
