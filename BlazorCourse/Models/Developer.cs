7e1968b58571974dbef0d7dbd6bf771bbf805d12
Lukam90 <lucien.hamm@gmail.com>
2023-07-05 21:42:44 +0200

    MAJ Nv Ex MudBlazor

--- BlazorCourse/Models/Developer.cs
+++
@@ -1,9 +0,0 @@
-namespace BlazorCourse.Models;
-
-public class Developer
-{
-    public Guid Id { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Company { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-}
