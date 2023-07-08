e6169b6cfe7eafdc0f51acec7bb2037d9c76e961
Lukam90 <lucien.hamm@gmail.com>
2023-07-08 13:28:49 +0200

    MAJ Nv Ex Angular

--- MudBlazorApp/Server/Models/Dto/CreateSupplierDto.cs
+++
@@ -1,8 +0,0 @@
-namespace MudBlazorApp.Server.Models.Dto;
-
-public class CreateSupplierDto
-{
-    public string Name { get; set; } = string.Empty;
-    public string Company { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-}
