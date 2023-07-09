a00d9d6b92e47a7c1f0ff24d1a5afb42dd0f88dd
Lukam90 <lucien.hamm@gmail.com>
2023-07-09 20:06:43 +0200

    MAJ Ex MVC > Edit

--- MvcApp/Controllers/CategoryController.cs
+++ MvcApp/Controllers/CategoryController.cs
@@ -47,4 +47,43 @@ public class CategoryController : Controller
 
         return View(category);
     }
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _db.Categories.Find(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder can't exactly match the Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(category);
+    }
 }
